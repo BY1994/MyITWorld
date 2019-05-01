d69957431fc3d45f9b335e62dbb2ab0478671360
BY1994 <gywlsangel@naver.com>
2019-05-01 23:49:54 +0900

    Initialize html and js files
    First commit
    Make HTML and JS files

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+/*
+//    const imageUrl = response.data.message
+//    const imageBox = document.querySelector('#img-div')
+//
+//    const image = document.createElement('img')
+//    image.src = imageUrl
+//    imageBox.appendChild(image)
+*/
